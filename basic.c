fc6a06e2aadef2955e3f3a3e20b547be25211fb2
CHANDRUGAYLE <chandrugayle496@gmail.com>
2024-06-14 10:05:49 +0530

    fix:mul 2 num of code

--- basic.c
+++ basic.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a=90,b=6;
+    int a=16,b=6;
     printf("Hello world");
-    printf("Multiplication is:%d",(a+b));
+    printf("Multiplication is:%d",(a*b));
     return 0;
 }
